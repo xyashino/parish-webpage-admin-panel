64260608b58bca059af95115931f7b3a02f3c523
xyashino <szczypiorpan123@gmail.com>
2023-02-19 19:42:48 +0100

    Change app layout

--- src/components/ui/AppLayout.tsx
+++ src/components/ui/AppLayout.tsx
@@ -1,13 +1,19 @@
 import { AppMenu } from "@components/AppMenu/AppMenu";
 import { Outlet } from "react-router-dom";
+import {Footer} from "@components/ui/Footer";
 
 export const AppLayout = () => {
   return (
-    <div className="flex w-screen bg-base-300">
-      <AppMenu />
-      <main className="flex flex-grow items-start justify-center  overflow-y-scroll">
-        <Outlet />
-      </main>
-    </div>
-  );
+      <div className='min-h-screen flex flex-col'>
+          <div className="flex w-screen bg-base-300 grow">
+              <AppMenu />
+              <main className="flex grow items-start justify-center  overflow-y-scroll">
+                  <Outlet />
+              </main>
+          </div>
+          <Footer/>
+      </div>
+
+
+);
 };
