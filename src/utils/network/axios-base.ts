cd0ae2e140498f1b1756a91cc885214ec2128506
xyashino <szczypiorpan123@gmail.com>
2023-02-25 16:09:28 +0100

    Add withCredentials option to base request.

--- src/utils/network/axios-base.ts
+++ src/utils/network/axios-base.ts
@@ -4,6 +4,6 @@ export const AxiosBase = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
   headers: {
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin":"*"
   },
+  withCredentials:true
 });
