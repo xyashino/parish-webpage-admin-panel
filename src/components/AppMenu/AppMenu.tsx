5d29fb337042fbac4a117cbce55ce41fd89f4c31
xyashino <szczypiorpan123@gmail.com>
2023-02-27 11:05:25 +0100

    Menu header redirect to home page

--- src/components/AppMenu/AppMenu.tsx
+++ src/components/AppMenu/AppMenu.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Logo } from "@icons/Logo";
 import { AppMenuItem } from "@components/AppMenu/AppMenuItem";
 import { MENU_STRUCTURE } from "@data/menu-structure.data";
+import {Link} from "react-router-dom";
+import {PageRouter} from "@enums/page-router.enum";
 
 export const AppMenu = () => {
   return (
     <nav className="flex  w-1/6 flex-col bg-base-100 shadow">
-      <div className="text-md flex items-center bg-primary p-4 font-bold uppercase text-base-100 shadow">
+      <Link to={PageRouter.Home} className="text-md flex items-center bg-primary p-4 font-bold uppercase text-base-100 shadow hover:bg-primary-focus">
         <Logo className="mr-2 text-3xl" />
         <h2>Parafia Gruszów wielki</h2>
-      </div>
+      </Link>
       <ul className="menu bg-base-100">
         {MENU_STRUCTURE.map(({ name, items }) => (
           <AppMenuItem name={name} items={items} key={name} />
