0988f213123ab4a95be379e7d5e3eb787d48c819
xyashino <szczypiorpan123@gmail.com>
2023-02-27 19:31:05 +0100

    Fix styles

--- src/components/ui/Modal/ModalBox.tsx
+++ src/components/ui/Modal/ModalBox.tsx
@@ -8,8 +8,8 @@ interface Props extends PropsWithChildren {
 export const ModalBox = ({ children,hideModal }: Props) => {
 
   return (
-      <div className='w-4/5 min-h-1/3 bg-white flex flex-col justify-center rounded'>
-          <header className='flex flex-row-reverse w-full pt-4 px-4 mb-4'>
+      <div className='w-2/5 min-h-1/3 bg-white flex flex-col justify-center rounded'>
+          <header className='flex flex-row-reverse w-full pt-4 px-8'>
                 <Close className='text-4xl hover:scale-125' onClick={hideModal}/>
                 <div className='grow m-4 border-b-2 align-self-start'></div>
           </header>
