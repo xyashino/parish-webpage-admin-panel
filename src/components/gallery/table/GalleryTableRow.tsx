60f2f17518119fc5d82316e40d3559e2d583754c
xyashino <szczypiorpan123@gmail.com>
2023-03-07 19:22:04 +0100

    Add click handler to open new page when clicking on table row

--- src/components/gallery/table/GalleryTableRow.tsx
+++ src/components/gallery/table/GalleryTableRow.tsx
@@ -1,31 +1,41 @@
-import React from "react";
+import React, {SyntheticEvent} from "react";
 import { BaseTableRow } from "@components/ui/Table/BaseTableRow";
-import {Modal} from "@components/ui/Modal/Modal";
-import {useModal} from "@hooks/useModal";
-import {RemoveGallery} from "@components/gallery/RemoveGallery";
+import { Modal } from "@components/ui/Modal/Modal";
+import { useModal } from "@hooks/useModal";
+import { RemoveGallery } from "@components/gallery/RemoveGallery";
+import {useNavigate} from "react-router-dom";
+import {PageRouter} from "@enums/page-router.enum";
 
 interface Props {
   index: number;
   title: string;
   id: string;
-    name: string | undefined;
+  name: string | undefined;
 }
 
 export const GalleryTableRow = ({ index, id, title, name }: Props) => {
-  const {hideModal,showModal,displayModal} = useModal()
+  const { hideModal, showModal, displayModal } = useModal();
+  const navigate = useNavigate();
+
+  const handleClick = (e:SyntheticEvent) => {
+    e.preventDefault();
+    navigate(`${PageRouter.Albums}/${id}`)
+  }
+
   return (
     <>
-      <BaseTableRow index={index} iconClick={displayModal}>
-        <td className="truncate">{id}</td>
-        <td>{title}</td>
-        <td>{name ?? "BRAK"}</td>
-      </BaseTableRow>
+        <BaseTableRow index={index} iconClick={displayModal} onClick={handleClick}>
+          <td className="truncate">{id}</td>
+          <td>{title}</td>
+          <td>{name ?? "BRAK"}</td>
+        </BaseTableRow>
+
       <Modal
-          hideModal={hideModal}
-          showModal={showModal}
-          boxModalClasses="w-2/5"
+        hideModal={hideModal}
+        showModal={showModal}
+        boxModalClasses="w-2/5"
       >
-        <RemoveGallery id={id} name={title} hideModal={hideModal}/>
+        <RemoveGallery id={id} name={title} hideModal={hideModal} />
       </Modal>
     </>
   );
