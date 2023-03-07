9acfa576c3d8b7d50bc5e618b8d9562b8c80f33a
xyashino <szczypiorpan123@gmail.com>
2023-03-07 19:20:51 +0100

    Add stopPropagation to click handler

--- src/components/ui/Table/BaseTableRow.tsx
+++ src/components/ui/Table/BaseTableRow.tsx
@@ -1,4 +1,4 @@
-import React, {HTMLAttributes, PropsWithChildren} from "react";
+import React, {HTMLAttributes, PropsWithChildren, SyntheticEvent} from "react";
 import {Trash} from "@icons/Trash";
 
 interface Props extends PropsWithChildren, HTMLAttributes<HTMLTableRowElement>{
@@ -8,12 +8,18 @@ interface Props extends PropsWithChildren, HTMLAttributes<HTMLTableRowElement>{
 
 export const BaseTableRow = ({index,iconClick,children, ...props}: Props) => {
     const iconStyles = "text-xl m-2 hover:scale-125";
+
+    const handleClick = (e:SyntheticEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        iconClick()
+    }
     return (
       <tr className="hover cursor-pointer transition-colors" {...props}>
           <td>{index + 1}</td>
           {children}
           <td className="flex">
-              <Trash className={iconStyles} onClick={iconClick} />
+              <Trash className={iconStyles} onClick={handleClick} />
           </td>
       </tr>
   );
