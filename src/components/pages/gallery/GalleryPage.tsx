4a494b45cb149b0d68a851dacf4eba90adc512a2
xyashino <szczypiorpan123@gmail.com>
2023-03-07 17:09:38 +0100

    Add modal logic

--- src/components/pages/gallery/GalleryPage.tsx
+++ src/components/pages/gallery/GalleryPage.tsx
@@ -6,16 +6,26 @@ import {useLoaderData} from "react-router-dom";
 import {BorderContainer} from "@components/ui/BorderContainer";
 import {Album} from "@backendTypes";
 import {GalleryTable} from "@components/gallery/table/GalleryTable";
+import {Modal} from "@components/ui/Modal/Modal";
+import {useModal} from "@hooks/useModal";
+import {AddGallery} from "@components/gallery/add/AddGallery";
 
 export const GalleryPage = () => {
     const data = useLoaderData() as Album[];
+    const {showModal,hideModal,displayModal}= useModal();
+    console.log(data)
     return (
-        <MainContainer>
-            <Header title='Zarządzaj galerią'/>
-            <BorderContainer>
-                <Btn className='btn-wide btn m-4' >Dodaj Galerie</Btn>
-            </BorderContainer>
-            <GalleryTable data={data}/>
-        </MainContainer>
+       <>
+           <MainContainer>
+               <Header title='Zarządzaj galerią'/>
+               <BorderContainer>
+                   <Btn className='btn-wide btn m-4' onClick={displayModal} >Dodaj Galerie</Btn>
+               </BorderContainer>
+               <GalleryTable data={data}/>
+           </MainContainer>
+           <Modal hideModal={hideModal} showModal={showModal} boxModalClasses='lg:w-3/4'>
+               <AddGallery hideModal={hideModal}/>
+           </Modal>
+       </>
     );
 };
