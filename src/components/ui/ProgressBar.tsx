b3eabf88b14cb2f4f59faf2e1957f768b3a56e20
xyashino <szczypiorpan123@gmail.com>
2023-03-12 20:45:57 +0100

    Implement option to show element description in ProgressBar

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -1,7 +1,15 @@
 interface Props {
-    value:number
+    value:number;
+    description?:true;
 }
 
-export const ProgressBar =({value}:Props)=>{
+export const ProgressBar =({value,description}:Props)=>{
+    if(description) {
+        return <div className='flex flex-col items-center'>
+            <progress className="progress w-56" value={value} max="100"></progress>
+            <p>{value === 100 ? 'Success !' : `${value} %`}</p>
+        </div>
+
+    }
     return <progress className="progress w-56" value={value} max="100"></progress>
 }
