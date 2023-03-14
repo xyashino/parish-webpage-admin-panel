5b95ba838267286a20da3b6ffe9192f8816050e1
xyashino <szczypiorpan123@gmail.com>
2023-03-14 12:28:00 +0100

    Add margin to base button for better spacing

--- src/components/ui/Btn.tsx
+++ src/components/ui/Btn.tsx
@@ -4,7 +4,7 @@ interface Props extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {
   addClasses?: string;
 }
 export const Btn = ({ className, children, addClasses, ...props }: Props) => {
-  const localStyles = "btn btn-primary m-1 p-2" + (addClasses ?? "");
+  const localStyles = "btn btn-primary m-2 p-2 my-4" + (addClasses ?? "");
   return (
     <button className={`${className ?? localStyles} text-base-100`} {...props}>
       {children}
