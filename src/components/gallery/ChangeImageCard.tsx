e56af2610b6bb9510a43f1d05f0cc59895d575ac
xyashino <szczypiorpan123@gmail.com>
2023-03-14 13:00:08 +0100

    Implement new component for background album image: ChangeImageCard

---
+++ src/components/gallery/ChangeImageCard.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction, SyntheticEvent } from "react";
+import { Album, Image } from "@backendTypes";
+import { useAxios } from "@hooks/useAxios";
+
+interface Props {
+  image: Image;
+  albumId: Album["id"];
+  src: string;
+  setBgcImg: Dispatch<SetStateAction<string | undefined>>;
+}
+
+export const ChangeImageCard = ({ image, albumId, src,setBgcImg }: Props) => {
+  const { url } = image;
+  const { fetchDataUsingAxios } = useAxios();
+  const handleClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await fetchDataUsingAxios(`albums/${albumId}`, {
+      method: "PATCH",
+      data: {
+        backgroundImage: url,
+      } as Album,
+    });
+    setBgcImg(url);
+  };
+
+  return (
+    <img
+      src={src}
+      onClick={handleClick}
+      alt={`${url}`}
+      className="rounded-box w-96 cursor-pointer  object-cover shadow-lg transition-transform hover:scale-125"
+    />
+  );
+};
