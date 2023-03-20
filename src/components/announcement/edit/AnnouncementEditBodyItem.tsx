b9d0fca6d05ef3f3740d0687597298e50503e001
xyashino <szczypiorpan123@gmail.com>
2023-03-20 15:34:29 +0100

    Introduce new logic in AnnouncementEditBodyItem component

--- src/components/announcement/edit/AnnouncementEditBodyItem.tsx
+++ src/components/announcement/edit/AnnouncementEditBodyItem.tsx
@@ -1,78 +1,68 @@
-import React, {ChangeEvent, useContext, useLayoutEffect, useState} from "react";
+import React, { ChangeEvent, useContext, useState } from "react";
 import { Divider } from "@components/ui/Divider";
 import { AnnouncementsItem } from "@backendTypes";
 import { AnnouncementContext } from "@context/AnnouncementContext";
 import { Close } from "@icons/Close";
+import parse from "html-react-parser";
+import { AnnouncementsAction } from "@enums/announcements-action.enum";
+import { Edit } from "@icons/Edit";
+import { useModal } from "@hooks/useModal";
+import {Modal} from "@components/ui/Modal/Modal";
+import {EditAnnouncementModalBody} from "@components/modal-body/EditAnnouncementModalBody";
 
 interface Props {
   item: AnnouncementsItem;
 }
 
 export const AnnouncementEditBodyItem = ({ item }: Props) => {
-  const { id: itemId, body: bodyItem, order: orderItem } = item;
-  const { setAnnouncements } = useContext(AnnouncementContext);
-
+  const { id, body, order: orderItem } = item;
+  const { dispatchAnnouncements } = useContext(AnnouncementContext);
+  const { hideModal, showModal, displayModal } = useModal();
   const [order, setOrder] = useState(orderItem);
-  const [body, setBody] = useState(bodyItem);
 
-  useLayoutEffect(() => {
-    const { body, order } = item;
-    setOrder(order);
-    setBody(body);
-  }, [item]);
   const updateValue = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    const { name, value } = e.target;
-    if (name === "order") {
-      setOrder(+value);
-      return;
-    }
-    setBody(value);
+    const {value} = e.target;
+    if( +value < 0 || +value > 100)return;
+    setOrder(+value);
   };
-
-  const updateDataInContext = () => {
-    setAnnouncements((prevState) => {
-      const { announcements } = prevState;
-      let itemIndex = announcements.findIndex(({ id }) => id === itemId);
-      if (itemIndex === -1) return prevState;
-      announcements[itemIndex] = { id: itemId, body, order };
-      return { ...prevState };
+  const updateDataInContext = (body:string) => {
+    dispatchAnnouncements({
+      type: AnnouncementsAction.UPDATE,
+      payload: { order , body ,id },
     });
   };
-
   const removeItemFromContext = () => {
-    setAnnouncements((prevState) => {
-      const { announcements } = prevState;
-      prevState.announcements = announcements.filter(({ id }) => id !== itemId);
-      return { ...prevState };
+    dispatchAnnouncements({
+      type: AnnouncementsAction.DELETE,
+      payload: { id },
     });
   };
-
   return (
-    <div>
-      <li className="flex w-full items-center">
-        <input
-          type="number"
-          value={order}
-          className="ghost input mx-2.5 w-1/12"
-          name="order"
-          onChange={(e) => updateValue(e)}
-          onBlur={updateDataInContext}
-        />
-        <textarea
-          name="body"
-          value={body}
-          className="textarea-primary textarea grow"
-          onChange={(e) => updateValue(e)}
-          onBlur={updateDataInContext}
-        />
-        <Close
-          className="mx-5 text-4xl text-primary hover:scale-125"
-          onClick={removeItemFromContext}
-        />
-      </li>
-      <Divider />
-    </div>
+    <>
+      <div>
+        <li className="flex w-full items-center">
+          <input
+            type="number"
+            value={order}
+            className="ghost input mx-2.5 w-1/12 h-full text-center text-xl"
+            name="order"
+            onChange={(e) => updateValue(e)}
+            onBlur={()=>updateDataInContext(body)}
+          />
+          <div className="w-4/5 border-l-2 px-2 my-2">{parse(body)}</div>
+          <Edit  onClick={displayModal} className="mx-5 text-3xl hover:scale-125 text-black"/>
+          <Close
+            className="mx-5 text-3xl hover:scale-125 text-black"
+            onClick={removeItemFromContext}
+          />
+        </li>
+        <Divider />
+      </div>
+      <Modal hideModal={hideModal} showModal={showModal}>
+        <EditAnnouncementModalBody hideModal={hideModal} body={body} addField={updateDataInContext}/>
+      </Modal>
+    </>
   );
 };
