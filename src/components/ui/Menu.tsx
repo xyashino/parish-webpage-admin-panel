446e491531ad485d19897a546edb609f3f120b89
xyashino <szczypiorpan123@gmail.com>
2023-03-25 16:17:41 +0100

    Create handleClick function with preventDefault and stopPropagation methods

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, SyntheticEvent } from "react";
 import { MenuItem } from "@frontendTypes/menu-item.interface";
 
 interface Props
@@ -7,12 +7,19 @@ interface Props
   onClick: (e: string) => void;
 }
 export const Menu = ({ items, onClick }: Props) => {
+  const handleClick = (e: SyntheticEvent, data: string) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (onClick) {
+      onClick(data);
+    }
+  };
   return (
     <ul className="menu menu-horizontal col-span-2  flex w-full flex-wrap justify-center bg-base-100">
-      {items.map(({ title, active, type, id }) => (
+      {items.map(({ title, active, id,type }) => (
         <li key={id ?? title} className="border-b-2xa">
           <button
-            onClick={() => (onClick ? onClick(type ?? title) : undefined)}
+            onClick={(e) => handleClick(e, type ?? id ?? title )}
             className={`bordered p-4 ${active ? "active" : ""}`}
           >
             {title}
