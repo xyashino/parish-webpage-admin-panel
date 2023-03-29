ae4e7b8529f46a744a58eb6bc50c1c8fb08aa05d
xyashino <szczypiorpan123@gmail.com>
2023-03-29 22:48:54 +0200

    Implement new RemoveGalleryModalBody component

--- src/components/gallery/table/GalleryTableRow.tsx
+++ src/components/gallery/table/GalleryTableRow.tsx
@@ -1,10 +1,10 @@
-import React, {SyntheticEvent} from "react";
+import React, { SyntheticEvent } from "react";
 import { BaseTableRow } from "@components/ui/Table/BaseTableRow";
 import { Modal } from "@components/ui/Modal/Modal";
 import { useModal } from "@hooks/useModal";
-import { RemoveGallery } from "@components/gallery/RemoveGallery";
-import {useNavigate} from "react-router-dom";
-import {PageRouter} from "@enums/page-router.enum";
+import { useNavigate } from "react-router-dom";
+import { PageRouter } from "@enums/page-router.enum";
+import { RemoveGalleryModalBody } from "@components/modal-body/RemoveGalleryModalBody";
 
 interface Props {
   index: number;
@@ -17,25 +17,29 @@ export const GalleryTableRow = ({ index, id, title, name }: Props) => {
   const { hideModal, showModal, displayModal } = useModal();
   const navigate = useNavigate();
 
-  const handleClick = (e:SyntheticEvent) => {
+  const handleClick = (e: SyntheticEvent) => {
     e.preventDefault();
-    navigate(`${PageRouter.Albums}/${id}`)
-  }
+    navigate(`${PageRouter.Albums}/${id}`);
+  };
 
   return (
     <>
-        <BaseTableRow index={index} iconClick={displayModal} onClick={handleClick}>
-          <td className="truncate">{id}</td>
-          <td>{title}</td>
-          <td>{name ?? "BRAK"}</td>
-        </BaseTableRow>
+      <BaseTableRow
+        index={index}
+        iconClick={displayModal}
+        onClick={handleClick}
+      >
+        <td className="truncate">{id}</td>
+        <td>{title}</td>
+        <td>{name ?? "BRAK"}</td>
+      </BaseTableRow>
 
       <Modal
         hideModal={hideModal}
         showModal={showModal}
         boxModalClasses="w-2/5"
       >
-        <RemoveGallery id={id} name={title} hideModal={hideModal} />
+        <RemoveGalleryModalBody id={id} name={title} hideModal={hideModal} />
       </Modal>
     </>
   );
