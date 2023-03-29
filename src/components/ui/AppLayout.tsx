b4528633a6e382d0abca8ef5ff0254b01a3e5588
xyashino <szczypiorpan123@gmail.com>
2023-03-29 22:17:11 +0200

    Improve AppLayout component styles to enhance visual design

--- src/components/ui/AppLayout.tsx
+++ src/components/ui/AppLayout.tsx
@@ -4,16 +4,14 @@ import {Footer} from "@components/ui/Footer";
 
 export const AppLayout = () => {
   return (
-      <div className='min-h-screen flex flex-col'>
+      <div className='flex flex-col h-screen'>
           <div className="flex w-screen bg-base-300 grow">
               <AppMenu />
-              <main className="flex items-start justify-center overflow-y-scroll  w-5/6">
+              <main className="flex items-start justify-center overflow-y-scroll w-5/6">
                   <Outlet />
               </main>
           </div>
           <Footer/>
       </div>
-
-
 );
 };
