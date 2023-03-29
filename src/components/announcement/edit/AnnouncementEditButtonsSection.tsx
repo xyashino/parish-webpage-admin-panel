109910eb86926d93080ada70dad8a2dec4bfb620
xyashino <szczypiorpan123@gmail.com>
2023-03-29 22:46:11 +0200

    Prettify code for enhanced readability and maintainability

--- src/components/announcement/edit/AnnouncementEditButtonsSection.tsx
+++ src/components/announcement/edit/AnnouncementEditButtonsSection.tsx
@@ -1,75 +1,103 @@
-import React, {useContext} from "react";
-import {Btn} from "@components/ui/Btn";
-import {PageRouter} from "@enums/page-router.enum";
-import {AnnouncementContext} from "@context/AnnouncementContext";
-import {CreateAnnouncementRequest} from "@backendTypes";
-import {useConfirmAlert} from "@hooks/useConfirmAlert";
-import {ConfirmAlert} from "@components/alerts/ConfirmAlert";
-import {useAxios} from "@hooks/useAxios";
-import {ErrorAlert} from "@components/alerts/ErrorAlert";
-import {BorderContainer} from "@components/ui/BorderContainer";
-import {useRevalidator} from "react-router-dom";
-import {AnnouncementsAction} from "@enums/announcements-action.enum";
+import React, { useContext } from "react";
+import { Btn } from "@components/ui/Btn";
+import { PageRouter } from "@enums/page-router.enum";
+import { AnnouncementContext } from "@context/AnnouncementContext";
+import { AnnouncementsItem, AnnouncementsResponse } from "@backendTypes";
+import { useConfirmAlert } from "@hooks/useConfirmAlert";
+import { ConfirmAlert } from "@components/alerts/ConfirmAlert";
+import { useAxios } from "@hooks/useAxios";
+import { ErrorAlert } from "@components/alerts/ErrorAlert";
+import { BorderContainer } from "@components/ui/BorderContainer";
+import { useRevalidator } from "react-router-dom";
+import { AnnouncementsAction } from "@enums/announcements-action.enum";
+import { SuccessAlert } from "@components/alerts/SuccessAlert";
+import { useSuccessAlert } from "@hooks/useSuccessAlert";
 
-export const AnnouncementEditBodyItem = () => {
-  const { announcements,restAnnouncement, setRestAnnouncement , dispatchAnnouncements } = useContext(AnnouncementContext);
+type RestAnnouncement = Omit<AnnouncementsResponse, "announcements" | "id"> & {
+  id?: string;
+};
+const createConfig = (
+  data: RestAnnouncement,
+  announcements: AnnouncementsItem[]
+) => {
+  const { id, status, ...rest } = data;
+  return {
+    method: id ? "patch" : "post",
+    data: {
+      ...rest,
+      status: (status as string) === "" ? null : status?.toUpperCase(),
+      announcements: announcements.map(({ body, order }) => ({
+        body,
+        order,
+      })),
+    },
+  };
+};
+
+export const AnnouncementEditButtonsSection = () => {
+  const {
+    announcements,
+    restAnnouncement,
+    setRestAnnouncement,
+    dispatchAnnouncements,
+  } = useContext(AnnouncementContext);
   const { setConfig, alertData } = useConfirmAlert();
-  const {revalidate} = useRevalidator();
+  const { showSuccess, isSuccess, hideSuccess } = useSuccessAlert();
+  const { revalidate } = useRevalidator();
   const {
     err: { data, hideError },
     fetchDataUsingAxios,
   } = useAxios();
-  const updateAnnouncements = async () => {
-    const {id,...rest}=restAnnouncement;
-    const config = {
-      method: "patch",
-      data: {
-        ...rest,
-        announcements: announcements.map(({ body, order }) => ({
-          body,
-          order,
-        })),
-      } as CreateAnnouncementRequest,
-    };
-    await fetchDataUsingAxios(`${PageRouter.Announcement}/${id}`, config);
+
+  const modifyAnnouncements = async () => {
+    const { id } = restAnnouncement;
+    const url = id
+      ? `${PageRouter.Announcement}${id}`
+      : PageRouter.Announcement;
+
+    await fetchDataUsingAxios(
+      url,
+      createConfig(restAnnouncement, announcements),
+      showSuccess
+    );
   };
 
-  const refreshData = async () => {
-    setConfig("Czy na pewno chcesz odświeżyć dane?", ()=>revalidate());
-  };
-
-  const updateData = async () => {
-    setConfig("Czy na pewno chcesz zaktualizować dane?", updateAnnouncements);
-  };
-
+  const refreshData = async () =>
+    setConfig("Czy na pewno chcesz odświeżyć dane?", () => revalidate());
+  const updateData = async () =>
+    setConfig("Czy na pewno chcesz zapisac zamiany ?", modifyAnnouncements);
   const clearData = async () => {
     setConfig("Czy na pewno chcesz wyczyścić dane?", () => {
-      dispatchAnnouncements({type:AnnouncementsAction.CLEAR,payload:{}})
-      setRestAnnouncement(({id}) => ({
-        id,
-        subtitle: "",
-        title: "",
-      }));
+      dispatchAnnouncements({ type: AnnouncementsAction.CLEAR, payload: {} });
+      setRestAnnouncement(({ id }) => ({ id, subtitle: "", title: "" }));
     });
   };
+
   return (
     <>
       <BorderContainer addClasses="flex w-full flex-wrap justify-between p-2 space-x-8">
         <Btn className="btn-wide btn" onClick={clearData}>
-          Wyczść Dane
+          Wyczść Wszystko
         </Btn>
-
         <Btn className="btn-wide btn" onClick={updateData}>
-          Aktualizuj Dane
+          Zapisz Zmiany
         </Btn>
-        <Btn className="btn-wide btn" onClick={refreshData}>
-          Odśwież Dane
-        </Btn>
+        {restAnnouncement.id ? (
+          <Btn className="btn-wide btn" onClick={refreshData}>
+            Odśwież Dane
+          </Btn>
+        ) : null}
       </BorderContainer>
-
       {alertData.show ? <ConfirmAlert config={alertData.config} /> : null}
       {data.show ? (
         <ErrorAlert onClick={hideError} message={data.message} />
+      ) : null}
+      {isSuccess ? (
+        <SuccessAlert
+          text="Sukces! Zmiany zostały zapisane"
+          hideMethod={hideSuccess}
+          hideAfterMs={1500}
+        />
       ) : null}
     </>
   );
