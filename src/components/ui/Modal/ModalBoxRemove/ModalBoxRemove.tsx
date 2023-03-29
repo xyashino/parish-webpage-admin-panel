fbfba8458177ead88d4afc97a6b7054ad214882f
xyashino <szczypiorpan123@gmail.com>
2023-03-29 22:40:50 +0200

    Refactor ModalBoxRemove component to validate data internally

--- src/components/ui/Modal/ModalBoxRemove/ModalBoxRemove.tsx
+++ src/components/ui/Modal/ModalBoxRemove/ModalBoxRemove.tsx
@@ -8,39 +8,50 @@ interface Props {
   hideModal: (e?: any) => void;
   mustTypeToDelete: string;
   loading: boolean;
-  showError: boolean;
+  isError: boolean;
+  showError: (message: string) => void;
   errorMessage: string;
   hideError: (e?: any) => void;
-  btnClick: (e: SyntheticEvent, value: string) => void;
+  removeMethod: () => void;
 }
 
 export const ModalBoxRemove = ({
   deleteInfoElement,
-  btnClick,
+  isError,
   loading,
   mustTypeToDelete,
   hideError,
   showError,
   errorMessage,
+  removeMethod,
 }: Props) => {
   const [inputValue, setValue] = useState("");
   const handleLoadingStyles = loading ? "loading" : "";
 
-  const ErrorElement = showError ? (
+  const ErrorElement = isError ? (
     <ErrorAlert onClick={hideError} message={errorMessage} />
   ) : null;
+
+  const mustType = mustTypeToDelete.trim().replaceAll(" ", "-");
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (mustType !== inputValue) {
+      showError("Podałeś zły teskt");
+      return
+    }
+    removeMethod();
+  };
 
   return (
     <div className="space-y-4 p-4 text-center">
       <p className="text-xl">{deleteInfoElement}</p>
       <p>
-        Żeby to zrobić przepisz{" "}
-        <span className="font-bold">{mustTypeToDelete}</span>
+        Żeby to zrobić przepisz <span className="font-bold">{mustType}</span>
       </p>
       <ModalBoxRemoveLabel inputValue={inputValue} setValue={setValue} />
       <Btn
         addClasses={`block btn-wide ml-4 ${handleLoadingStyles}`}
-        onClick={(e) => btnClick(e, inputValue)}
+        onClick={handleClick}
       >
         Usuń
       </Btn>
