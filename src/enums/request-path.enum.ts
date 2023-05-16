d2df1332cf35498394d634bbc4e12e0c53ba774e
xyashino <szczypiorpan123@gmail.com>
2023-05-16 14:39:56 +0200

    Refactor

--- src/enums/request-path.enum.ts
+++ src/enums/request-path.enum.ts
@@ -1,12 +1,11 @@
 export const enum RequestPath {
   OneAlbum = "/albums/",
   Albums = "/albums",
-  AlbumTypes = "/album/types",
+  AlbumTypes = "/albums/types",
   CurrentUser = "/users/current",
   Users = "/users",
   OneIntention = "/intentions/",
   Announcements = "/announcements/",
   Logout = "/auth/logout",
-
   Register='/users/register',
 }
